d7b50403b3b988cd8f9ae14eeb4b57f016cff945
George K. Paloulian <george.k.paloulian@gmail.com>
2021-06-27 23:50:45 -0700

    Formalized inclusion of GenericOAuthenticator

--- jupyter/jupyter_lab_config.py
+++ jupyter/jupyter_lab_config.py
@@ -1,5 +1,10 @@
-c.JupyterHub.authenticator_class = "generic"
-c.OAuthenticator.oauth_callback_url = "https://kubeflow.babylon2.beyond.ai/oauth2/callback"
+from oauthenticator.generic import GenericOAuthenticator
+
+c.JupyterHub.authenticator_class = GenericOAuthenticator
+
+# These should get picked up by env var
+#c.OAuthenticator.oauth_callback_url = "https://kubeflow.babylon2.beyond.ai/oauth2/callback" 
+
 c.OAuthenticator.client_id = "r8v72ivs9unbno3tgg3t0c2ki"
 c.OAuthenticator.client_secret = "m7nhrqvqqhlapip72584l07cq137qmac4mu61o9for24evlihf8"
 
