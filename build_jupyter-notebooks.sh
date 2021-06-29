27e770543f1c6090b94f00d58d9a63d60b2e64f2
George K. Paloulian <george.k.paloulian@gmail.com>
2021-06-29 12:11:01 -0700

    Added aws ecr login for pushes

--- build_jupyter-notebooks.sh
+++ build_jupyter-notebooks.sh
@@ -12,6 +12,7 @@ docker tag ai.beyond/babylon/notebooks/base:latest 562046374233.dkr.ecr.us-west-
 docker tag ai.beyond/babylon/notebooks/jupyter-base:latest 562046374233.dkr.ecr.us-west-2.amazonaws.com/ai.beyond/babylon/notebooks/jupyter-base:latest
 docker tag ai.beyond/babylon/notebooks/jupyter-scipy:latest 562046374233.dkr.ecr.us-west-2.amazonaws.com/ai.beyond/babylon/notebooks/jupyter-scipy:latest
 
+aws ecr get-login-password --region us-west-2 --profile bl-babylon | docker login --username AWS --password-stdin 562046374233.dkr.ecr.us-west-2.amazonaws.com
 docker push 562046374233.dkr.ecr.us-west-2.amazonaws.com/ai.beyond/babylon/notebooks/base:latest
 docker push 562046374233.dkr.ecr.us-west-2.amazonaws.com/ai.beyond/babylon/notebooks/jupyter-base:latest 
 docker push 562046374233.dkr.ecr.us-west-2.amazonaws.com/ai.beyond/babylon/notebooks/jupyter-scipy:latest
